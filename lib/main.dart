22b2a251e616a58d42d68ca81734516e8664dfbb
Fhanafii <fhanafi544@gmail.com>
2026-04-16 13:52:14 +0700

    feat: adding new route and provider for viewmodel in each screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:let_it_grow_app/features/auth/view/auth_gate.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:provider/provider.dart';
 import 'features/auth/viewmodel/auth_viewmodel.dart';
+import 'features/plant/viewmodel/plant_viewmodel.dart';
 import 'features/auth/view/login_screen.dart';
-
+import 'features/plant/view/setup_plant_screen.dart';
+import 'features/plant/view/home_screen.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,16 +15,20 @@ void main() async {
   await Supabase.initialize(
     url: dotenv.get('SUPABASE_URL'),
     anonKey: dotenv.get('SUPABASE_ANON_KEY'),
+    authOptions: const FlutterAuthClientOptions(
+      authFlowType: AuthFlowType.pkce,
+    )
   );
 
     runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthViewModel()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthViewModel()),
+          ChangeNotifierProvider(create: (_) => PlantViewModel()),
+        ],
+        child: MyApp(),
+      ),
+    );
 }
 
 class MyApp extends StatelessWidget {
@@ -32,8 +39,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Let It Grow',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: LoginScreen(), // Arahkan Langsung ke LoginScreen
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      initialRoute: '/',
+      routes:  {
+        '/': (context) => const AuthGate(),
+        '/login': (context) => LoginScreen(),
+        '/setup-plant': (context) => const SetupPlantScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
     );
   }
 }
