661944bd4565a436f63e04159c4e0380a6acc881
Fhanafii <fhanafi544@gmail.com>
2026-04-18 16:02:12 +0700

    feat: adding moisture history stream for chart functionality

--- lib/data/repositories/plant_repository.dart
+++ lib/data/repositories/plant_repository.dart
@@ -1,24 +1,27 @@
 // lib/data/repositories/plant_repository.dart
+import 'package:let_it_grow_app/features/plant/viewmodel/plant_viewmodel.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../features/plant/model/mqtt_account_model.dart';
+import '../../features/plant/model/plant_model.dart';
 
 class PlantRepository {
   final _supabase = Supabase.instance.client;
 
   // Memanggil RPC untuk generate akun MQTT
-  Future<Map<String, dynamic>> generateMqttAccount() async {
+   Future<MqttAccount> generateMqttAccount() async {
     // final response = await _supabase.rpc('generate_mqtt_credentials');
     // return response as Map<String, dynamic>;
     final List<dynamic> response  = await _supabase.rpc('generate_mqtt_credentials');
 
     if(response.isNotEmpty) {
-      return response[0] as Map<String, dynamic>;
+      return MqttAccount.fromJson(response[0] as Map<String, dynamic>);
     }else {
       throw Exception('Gagal mendapatkan data akun');
     }
   }
 
   // Memanggil register mqtt credentials untuk troubleshooting user
-  Future<Map<String, dynamic>?> getRegisteredCredentials() async {
+  Future<MqttAccount?> getRegisteredCredentials() async  {
     final userId = _supabase.auth.currentUser?.id;
     if (userId == null){
       throw Exception("Akses ditolak: Silakan login dengan Google terlebih dahulu.");
@@ -31,13 +34,34 @@ class PlantRepository {
           .eq('user_id', userId)
           .maybeSingle();
       
-      return response;
+      return response != null ? MqttAccount.fromJson(response) : null;
     } catch (e) {
       rethrow;
     }
   }
+  // Khusus untuk menampilkan data di chart
+   Stream<List<Map<String, dynamic>>> getMoistureHistoryStream(
+    String userId, {
+    ChartFilter filter = ChartFilter.today,
+  }) {
+    // 1. Tentukan waktu mulai berdasarkan filter
+    DateTime startDate;
+    if (filter == ChartFilter.month) {
+      startDate = DateTime.now().subtract(const Duration(days: 30));
+    } else {
+      startDate = DateTime.now().subtract(const Duration(hours: 24));
+    }
 
-  // Mendapatkan data sensor terbaru (Realtime)
+    // 2. Lakukan stream dengan filter waktu (gte = Greater Than or Equal)
+    return _supabase
+        .from('moisture_logs')
+        .stream(primaryKey: ['id'])
+        .eq('user_id', userId)
+        .order('created_at', ascending: true)
+        .limit(filter == ChartFilter.month ? 100 : 20); // Sesuaikan limit jika bulanan
+  }
+
+  // Mendapatkan data sensor terbaru (Realtime) untuk mengetahui apakah device user telah terkoneksi dan akan diarahkan langsung ke home screen jika berhasil
   Stream<List<Map<String, dynamic>>> getMoistureStream(String userId) {
     return _supabase
         .from('moisture_logs')
